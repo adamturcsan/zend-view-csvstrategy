81aa27ce5e03d58b7c1325dca562172f00f96cea
Turcsán Ádám <turcsan.adam@legow.hu>
2018-06-11 16:56:14 +0200

    - php7.0 is the new minimal php version - Add Module.php and ConfigProvider to support component-installer

---
+++ src/ConfigProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * LegoW\View\Strategy\CsvStrategy (https://github.com/adamturcsan/zend-view-csvstrategy)
+ * 
+ * @copyright Copyright (c) 2014-2018 Legow Hosting Kft. (http://www.legow.hu)
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+
+declare (strict_types = 1);
+
+namespace LegoW\View\Csv;
+
+use LegoW\View\Csv\Renderer\CsvRenderer;
+use LegoW\View\Csv\Renderer\CsvRendererFactory;
+use LegoW\View\Csv\Strategy\CsvStrategy;
+use LegoW\View\Csv\Strategy\CsvStrategyFactory;
+
+/**
+ * Description of ConfigProvider
+ *
+ * @author Turcsán Ádám <turcsan.adam@legow.hu>
+ */
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'service_manager' => [
+                'factories' => [
+                    CsvRenderer::class => CsvRendererFactory::class,
+                    CsvStrategy::class => CsvStrategyFactory::class
+                ]
+            ]
+        ];
+    }
+}
